481523fe7eb6cdf56355191b8cb200b2ba0d9858
Luan L <luan.rodrigues@terceiro.rnp.br>
2024-10-23 11:13:49 -0300

    chore: inclusao de variavel de ambiente para intervalo de refresh

--- public/assets/env/env.js
+++ public/assets/env/env.js
@@ -4,5 +4,6 @@ window.env = {
   KEYCLOAK_URL: 'https://keycloak.cloud.getinsight.tech',
   KEYCLOAK_REALM: 'access-pilot',
   KEYCLOAK_CLIENT_ID: 'accesspilot-frontend',
-  KEYCLOAK_REFRESH_TOKEN_TIME: 30
+  KEYCLOAK_REFRESH_TOKEN_TIME: 30,
+  DASHBOARD_REFRESH_INTERVAL: 5000
 }
