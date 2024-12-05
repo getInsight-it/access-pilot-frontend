5b10c5570134a6e7bca2d3a0c014a7892be121b8
Luan L <luan.rodrigues@terceiro.rnp.br>
2024-12-05 14:37:31 -0300

    chore:atualizando env com url do keyclock correta

--- public/assets/env/env.js
+++ public/assets/env/env.js
@@ -1,7 +1,7 @@
 window.env = {
-  API_URL: 'http://localhost:8080',
+  API_URL: 'http://localhost:8082',
   FRONTEND_URL: 'http://localhost:5173',
-  KEYCLOAK_URL: 'http://localhost:7080',
+  KEYCLOAK_URL: 'https://keycloak.cloud.getinsight.tech',
   KEYCLOAK_REALM: 'access-pilot',
   KEYCLOAK_CLIENT_ID: 'accesspilot-frontend',
   KEYCLOAK_REFRESH_TOKEN_TIME: 30,
