77ccf32de2ecf65f7d87c51615bb074fd7f5285e
gabriel barbosa <gabrielarraesbarbosa@gmail.com>
2025-09-25 14:51:40 -0300

    fix: dashboard layout imports

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../common/components/layout/header/header.tsx";
-import Sidebar from "../common/components/layout/sidebar/sidebar.tsx";
+import Header from "../common/components/layout/header/Header.tsx";
+import Sidebar from "../common/components/layout/sidebar/Sidebar.tsx";
 
 export default function DashboardLayout() {
   return (
